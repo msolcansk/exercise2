cceb73f6f9e321f5dfc245752d1903759e6ffa28
msolcansk <solcansky.martin@gmail.com>
2022-03-18 13:20:11 +0100

    add argparser

--- app/network_scanner.py
+++ app/network_scanner.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
+
 import nmap
+import argparse
 
 
 def print_scan(portscanner):
@@ -15,7 +18,12 @@ def print_scan(portscanner):
 
 def main():
 
-    target = '127.0.0.1'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('target', help='Provide ip or network subnet to be scanned')
+    args = parser.parse_args()
+    
+
+    target = args.target
     ps = nmap.PortScanner()
     ps.scan(hosts=target, arguments='')
     print_scan(ps)
